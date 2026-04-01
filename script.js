e71553b168b1c9d76437f0a096ad5741df78f26a
singlakinjal <thekinjalsingla@gmail.com>
2026-04-01 21:59:31 +0530

    updated js

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-const searchInput = document.getElementById("searchInput");
-const loading = document.getElementById("loading");
-const bookContainer = document.getElementById("book-grid")
-const url = "https://openlibrary.org/search.json"
+// const searchInput = document.getElementById("searchInput");
+// const loading = document.getElementById("loading");
+// const bookContainer = document.getElementById("book-grid")
+const url = "https://gutendex.com/books/";
+const bookContainer = document.querySelector(".books-container")
 
-async function fetchapi(query = "") {
-    loading.style.display = "block"
-    const response = await fetch(url + `?q=${query}`)
-    const data = await response.json()
-    loading.style.display = "none"
-    console.log(data)
-}
+function createCard(imgUrl, title, author) {
 
-searchInput.addEventListener("input", (e) => {
-    bookContainer.innerHTML = ""
-    fetchapi(e.target.value)
-})
-function renderBooks(data) {
-    return data.results.map((result) => {
-        <div class="book-card">
-            <img src="${cover}" />
-            <h3>${title}</h3>
-            <p>${author}</p>
-        </div>
-        // finish this
-    })
+    bookContainer.innerHTML += `<div class="book-card">
+            <img class="book-image" src=${imgUrl} alt="book of ${author}">
+            <div class="book-title">${title}</div>
+            <div class="book-author">${author}</div>
+        </div>`
 }
+async function fetchapi(url) {
+    try {
+
+        const response = await fetch(url)
+        const data = await response.json()
+        const base = data.results
+
+        a = base.map((result) => {
+            const imgUrl = result.formats["image/jpeg"];
+            const title = result.title;
+            const author = result.authors[0] ? result.authors[0].name : "Unknown Author"
+            console.log(imgUrl, title, author);
+            createCard(imgUrl, title, author)
+
+        })
+    } catch (error) {
+bookContainer.innerHTML = `<p>Error loading the archive.</p>`;
+    } 
+
+}
+fetchapi(url)
+
+
+
