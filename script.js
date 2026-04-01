743e5b84237cf1960c5d1aab90f05e55eae9796b
singlakinjal <thekinjalsingla@gmail.com>
2026-04-01 23:12:55 +0530

    added dark mode and loading

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
-// const searchInput = document.getElementById("searchInput");
-// const loading = document.getElementById("loading");
-// const bookContainer = document.getElementById("book-grid")
+// Dark mode
+const themeToggle = document.getElementById("themeToggle");
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    const isDark = document.body.classList.contains("dark-mode");
+    themeToggle.textContent = isDark ? "☀️" : "🌙";
+});
+
+// api fetch
 const url = "https://gutendex.com/books/";
 const bookContainer = document.querySelector(".books-container")
+const loadingElement = document.getElementById("loading")
 
 function createCard(imgUrl, title, author) {
 
-    bookContainer.innerHTML += `<div class="book-card">
+    return  `<div class="book-card">
             <img class="book-image" src=${imgUrl} alt="book of ${author}">
             <div class="book-title">${title}</div>
             <div class="book-author">${author}</div>
         </div>`
 }
 async function fetchapi(url) {
+    if (loading) loading.style.display = "flex";
+    bookContainer.innerHTML = "";
     try {
 
         const response = await fetch(url)
         const data = await response.json()
         const base = data.results
 
-        a = base.map((result) => {
+        bookContainer.innerHTML = base.map((result) => {
             const imgUrl = result.formats["image/jpeg"];
             const title = result.title;
             const author = result.authors[0] ? result.authors[0].name : "Unknown Author"
-            console.log(imgUrl, title, author);
-            createCard(imgUrl, title, author)
+            // console.log(imgUrl, title, author);
+            // createCard(imgUrl, title, author)
+            return createCard(imgUrl, title, author);
+        }).join("");
 
-        })
+    
     } catch (error) {
 bookContainer.innerHTML = `<p>Error loading the archive.</p>`;
-    } 
+    } finally {
+        if (loadingElement) loadingElement.style.display = "none"; // Hide loading
+    }
 
 }
 fetchapi(url)
